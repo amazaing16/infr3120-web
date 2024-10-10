ff3be8a62798c8e94af47360fca8eef09539ee46
amazaing16 <zain.garuba@ontariotechu.net>
2024-10-10 16:40:56 -0400

    Add files via upload

---
+++ project03-02.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-02
+
+      Application to generate a slide gallery
+      Author: 
+      Date:   
+
+      Filename: project03-02.js
+*/
+
